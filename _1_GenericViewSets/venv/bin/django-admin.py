63511256ccba29b40c84453885c8f7b07a269eb7
SameeranB <sameeranbandishti@ieee.org>
2020-07-02 00:16:08 +0530

    GenericViewSets-An Introduction

--- _1_GenericViewSets/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/caronex/Tech/CaronexLabs/DjangoRestForNotBeginners/_1_GenericViewSets/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
